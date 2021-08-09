72f4464cff50bfe22af84f6b7386565e0eb9fef1
EirkinDahi <eirkin12@gmail.com>
2021-08-08 23:55:13 -0400

    add basic routing

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import express  from 'express';
 import bodyParser from 'body-parser';
 
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    res.send("Hello");
+    console.log('[Test!]');
+});
+app.listen(PORT,()=>console.log(`Server is running on port: http://localhost:${PORT}`));
