869d6e6097bef80a1a83bc9605a15fb28b8a2f3a
EirkinDahi <eirkin12@gmail.com>
2021-08-10 23:56:06 -0400

    add post route for adding user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,23 @@
 import express from 'express';
 
 const router = express.Router();
+const users = [
+    {
+        firstname: "Eirkin",
+        lastname: "Dahi",
+        age: 20
+    }
+]
 
 //routes here start with /user
 router.get('/',(req,res)=>{
-    res.send("Hellow from users");
+    res.send(users);
+});
+
+router.post('/',(req,res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(`User with the username ${user.firstname} added`);
 });
 
 export default router;
